ceada76d8db2e5e94c552668008d51c04a9f8bfd
JagatheshwaranN <jagatheshwaran.n29@gmail.com>
2023-06-16 13:57:29 +0530

    DB Concepts - Update & Delete Records

---
+++ src/main/java/dml/non_select/UpdateRecord.java
@@ -0,0 +1,21 @@
+package dml.non_select;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class UpdateRecord {
+
+    public static void main(String[] args) throws ClassNotFoundException {
+
+        Class.forName("oracle.jdbc.driver.OracleDriver");
+        try(Connection connection = DriverManager.getConnection("jdbc:oracle:thin:@//localhost:1521/XEPDB1", "JAGA", "ALLOWME")){
+            Statement statement = connection.createStatement();
+            int updateCount = statement.executeUpdate("Update Groceries Set Grcs_No = 101 where Grcs_Name = 'Cookies'");
+            System.out.println("Number of rows updated : "+updateCount);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
