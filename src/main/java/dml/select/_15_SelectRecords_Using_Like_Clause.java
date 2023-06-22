6d42deb497ba81f099630cbeb1aab02cc3129ff8
JagatheshwaranN <jagatheshwaran.n29@gmail.com>
2023-06-22 15:35:43 +0530

    DB Concepts - Null, Like Clauses

---
+++ src/main/java/dml/select/_15_SelectRecords_Using_Like_Clause.java
@@ -0,0 +1,33 @@
+package dml.select;
+
+import java.sql.*;
+
+public class _15_SelectRecords_Using_Like_Clause {
+    static ResultSet resultSet;
+    public static void main(String[] args) throws ClassNotFoundException {
+
+        Class.forName("oracle.jdbc.driver.OracleDriver");
+        try(Connection connection = DriverManager.getConnection("jdbc:oracle:thin:@//localhost:1521/XEPDB1", "JAGA", "ALLOWME")){
+            Statement statement = connection.createStatement();
+            resultSet = statement.executeQuery("Select * from Emp where Ename Like 'S%'");
+            while (resultSet.next()){
+                System.out.println(resultSet.getInt(1)+" | "+resultSet.getString(2)+" | "+
+                        resultSet.getInt(3) + " | "+resultSet.getString(4));
+            }
+            System.out.println("=====================================");
+            resultSet = statement.executeQuery("Select * from Emp where Ename Like '%i%'");
+            while (resultSet.next()){
+                System.out.println(resultSet.getInt(1)+" | "+resultSet.getString(2)+" | "+
+                        resultSet.getInt(3) + " | "+resultSet.getString(4));
+            }
+            System.out.println("=====================================");
+            resultSet = statement.executeQuery("Select * from Emp where Ename Like '____'");
+            while (resultSet.next()){
+                System.out.println(resultSet.getInt(1)+" | "+resultSet.getString(2)+" | "+
+                        resultSet.getInt(3) + " | "+resultSet.getString(4));
+            }
+        } catch (SQLException sqlException) {
+            throw new RuntimeException(sqlException);
+        }
+    }
+}
