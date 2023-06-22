8008247bf2acf9d2d6c40a864267c6e430177312
JagatheshwaranN <jagatheshwaran.n29@gmail.com>
2023-06-22 15:37:53 +0530

    DB Concepts - Null, Like Clauses

--- src/main/java/dml/select/_10_SelectRecords_Using_Relational_Ops_OrderBy_Type1.java
+++ src/main/java/dml/select/_10_SelectRecords_Using_Relational_Ops_OrderBy_Type1.java
@@ -9,7 +9,7 @@ public class _10_SelectRecords_Using_Relational_Ops_OrderBy_Type1 {
         Class.forName("oracle.jdbc.driver.OracleDriver");
         try(Connection connection = DriverManager.getConnection("jdbc:oracle:thin:@//localhost:1521/XEPDB1", "JAGA", "ALLOWME")){
             Statement statement = connection.createStatement();
-            ResultSet resultSet = statement.executeQuery("Select * from Emp where Designation != 'Developer' order by Sal");
+            ResultSet resultSet = statement.executeQuery("Select * from Emp where Designation = 'Developer' order by Sal");
             while (resultSet.next()){
                 System.out.println(resultSet.getInt(1)+" | "+resultSet.getString(2)+" | "+
                         resultSet.getInt(3) + " | "+resultSet.getString(4));
