8721f497190dcd29fa1f1e433a664ac257788c5e
JagatheshwaranN <jagatheshwaran.n29@gmail.com>
2023-06-23 11:31:01 +0530

    DB Concepts - Select using Logical Operators

---
+++ src/main/java/dml/select/logical_ops/_02_Select_Records_Using_Or_Operator.java
@@ -0,0 +1,22 @@
+package dml.select.logical_ops;
+
+import java.sql.*;
+
+public class _02_Select_Records_Using_Or_Operator {
+
+    public static void main(String[] args) throws ClassNotFoundException {
+
+        Class.forName("oracle.jdbc.driver.OracleDriver");
+        try(Connection connection = DriverManager.getConnection("jdbc:oracle:thin:@//localhost:1521/XEPDB1", "JAGA", "ALLOWME")){
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("Select * From Emp Where Designation = 'Tester' Or Sal >= 40000");
+            while (resultSet.next()){
+                System.out.println(resultSet.getInt(1)+" | "+resultSet.getString(2)+" | "+
+                        resultSet.getInt(3) + " | "+resultSet.getString(4));
+            }
+        } catch (SQLException sqlException) {
+            throw new RuntimeException(sqlException);
+        }
+
+    }
+}
