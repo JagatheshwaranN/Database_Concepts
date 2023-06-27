0615cf67146a07fe53ec3e6f85388d5853492707
JagatheshwaranN <jagatheshwaran.n29@gmail.com>
2023-06-27 14:51:33 +0530

    DB Concepts - Alter Table Scenarios Updated

--- src/main/java/ddl/_02_AlterTable.java
+++ src/main/java/ddl/_02_AlterTable.java
@@ -7,26 +7,53 @@ import java.sql.Statement;
 
 public class _02_AlterTable {
 
+    static Statement statement;
+
     public static void main(String[] args) throws ClassNotFoundException {
 
-        /*
-            Alter Table has 2 options as below,
-
-            1. Modify existing column details
-               Example - Alter Table Groceries Modify (Grcs_No number(5,0), Grcs_Name varchar2(25), Grcs_Qty varchar2(5), Grcs_Price number(7,2));
-
-            2. Add new column
-               Example - Alter Table Groceries Add (Grcs_No number(5,0), Grcs_Name varchar2(25), Grcs_Qty varchar2(5), Grcs_Price number(7,2), Grcs_Type varchar2(20));
-        */
-
+        createTable();
         Class.forName("oracle.jdbc.driver.OracleDriver");
         try(Connection connection = DriverManager.getConnection("jdbc:oracle:thin:@//localhost:1521/XEPDB1", "JAGA", "ALLOWME")){
-            Statement statement = connection.createStatement();
-            statement.executeUpdate("Alter Table Groceries Modify (Grcs_No number(5,0), Grcs_Name varchar2(25), Grcs_Qty varchar2(5), Grcs_Price number(7,2))");
-            System.out.println("Groceries Table altered.");
+            statement = connection.createStatement();
+
+            statement.executeUpdate("Alter Table Movie Modify (Name varchar2(25), Director varchar2(25), Crew number(5))");
+            System.out.println("Movie Table altered with modify function.");
+
+            statement.executeUpdate("Alter Table Movie Add (Production varchar2(25))");
+            System.out.println("Movie Table altered with add function.");
+
+            statement.executeUpdate("Alter Table Movie Drop Column Production");
+            System.out.println("Movie Table altered with drop function.");
+
+            statement.executeUpdate("Alter Table Movie Rename Column Name To MovieName");
+            System.out.println("Movie Table Column altered with Rename function.");
+
+            statement.executeUpdate("Alter Table Movie Rename To Movies");
+            System.out.println("Movie Table altered with Rename function.");
+
         } catch (SQLException sqlException) {
             throw new RuntimeException(sqlException);
         }
+        finally {
+            try {
+            Connection connection = DriverManager.getConnection("jdbc:oracle:thin:@//localhost:1521/XEPDB1", "JAGA", "ALLOWME");
+            statement = connection.createStatement();
+            statement.executeUpdate("Drop Table Movies");
+            System.out.println("Movies Table dropped.");
+            } catch (SQLException sqlException) {
+                sqlException.printStackTrace();
+            }
+        }
+    }
 
+    private static void createTable() throws ClassNotFoundException {
+        Class.forName("oracle.jdbc.driver.OracleDriver");
+        try (Connection connection = DriverManager.getConnection("jdbc:oracle:thin:@//localhost:1521/XEPDB1", "JAGA", "ALLOWME")) {
+            Statement statement = connection.createStatement();
+            statement.executeUpdate("Create Table Movie(Name varchar2(20), Director varchar2(20), Crew number(2))");
+            System.out.println("Movie Table created.");
+        } catch (SQLException sqlException) {
+            throw new RuntimeException(sqlException);
+        }
     }
 }
