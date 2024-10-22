52158a0627a3f7042ef2a1e2eab3ca9a4ab5b225
JagatheshwaranN <jagatheshwaran.n29@gmail.com>
2024-10-22 20:25:01 +0530

    DB - PostgresSQL Notes Updated

--- src/test/resources/PostgresSql/sales_order_case_study.sql
+++ src/test/resources/PostgresSql/sales_order_case_study.sql
@@ -13,4 +13,24 @@ select status from sales_order where status not in ('Completed', 'completed');
 select status from sales_order where lower(status) <> 'completed';
 select status from sales_order where upper(status) <> 'COMPLETED';
 
+-- 3) Display the order id, order date, and product name for all the completed orders.
+select order_id, order_date, name
+from sales_order so inner join products p on p.id = so.prod_id 
+where lower(so.status) = 'completed';
+
+-- 4) Sort the above query to show the earliest orders at the top. Also display the
+-- customer who purchased these orders.
+select order_id, order_date, p.name, c.name
+from sales_order so 
+inner join products p on p.id = so.prod_id 
+inner join customers c on so.customer_id = c.id
+where lower(so.status) = 'completed'
+order by order_date asc;
+
+-- 5) Display the total number of orders corresponding to each delivery status.
+select status, count(*)
+from sales_order
+group by status
+
+
 
