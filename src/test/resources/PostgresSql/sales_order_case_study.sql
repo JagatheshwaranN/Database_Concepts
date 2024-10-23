edf4782c2b2af401465bbf3417133698166211dd
JagatheshwaranN <jagatheshwaran.n29@gmail.com>
2024-10-23 20:26:11 +0530

    DB - PostgresSQL Notes Updated

--- src/test/resources/PostgresSql/sales_order_case_study.sql
+++ src/test/resources/PostgresSql/sales_order_case_study.sql
@@ -28,9 +28,46 @@ where lower(so.status) = 'completed'
 order by order_date asc;
 
 -- 5) Display the total number of orders corresponding to each delivery status.
-select status, count(*)
+select status, count(*) as order_total
 from sales_order
 group by status
 
+-- 6) For Orders purchased more than 1 item, how many are still not completed.
+select count(*) as not_completed_orders
+from sales_order
+where quantity > 1
+and lower(status) <> 'completed';
+
+-- 7) Find the total number of orders corresponding to each delivery status by
+-- ignoring the case in delivery status with highesh number of orders should be
+-- at the top.
+
+-- Approach 1 [Sub Query]
+select status, 
+case when status = 'completed'
+then 'Completed'
+else status
+end as updated_status from sales_order;
+
+select updated_status, count(*) as order_total
+from (select status, 
+case when status = 'completed'
+then 'Completed'
+else status
+end as updated_status from sales_order)
+group by updated_status
+order by order_total desc;
+
+-- Approach 2
+select lower(status) as status, count(*) as order_total
+from sales_order
+group by lower(status)
+order by order_total desc;
+
+-- 8) Write a query to identify the total products purchased by each customer.
+select c.name as customer_name, sum(so.quantity)
+from sales_order so 
+join customers c on c.id = so.customer_id
+group by c.name;
 
 
