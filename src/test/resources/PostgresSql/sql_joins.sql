9d46fad69f6014727ec50356b3079ca88d2172d0
JagatheshwaranN <jagatheshwaran.n29@gmail.com>
2024-11-01 09:31:18 +0530

    DB - PostgresSQL JOINS Notes Updated

--- src/test/resources/PostgresSql/sql_joins.sql
+++ src/test/resources/PostgresSql/sql_joins.sql
@@ -50,10 +50,38 @@ insert into JProjects values
 ('P2', 'ETL Tool', 'E1'),
 ('P2', 'ETL Tool', 'M4');
 
+create table JCompany
+(
+	company_id		varchar(10),
+	company_name	varchar(50),
+	location		varchar(20)
+);
+insert into JCompany values
+('C001', 'tech Solutions', 'Kuala Lumpur');
+
+CREATE TABLE family
+(
+    member_id     VARCHAR(10),
+    name          VARCHAR(50),
+    age           INT,
+    parent_id     VARCHAR(10)
+);
+insert into family values
+  ('F1', 'David', 4, 'F5'),
+  ('F2', 'Carol', 10, 'F5'),
+  ('F3', 'Michael', 12, 'F5'),
+  ('F4', 'Johnson', 36, ''),
+  ('F5', 'Maryam', 40, 'F6'),
+  ('F6', 'Stewart', 70, ''),
+  ('F7', 'Rohan', 6, 'F4'),
+  ('F8', 'Asha', 8, 'F4');
+
 select * from JEmployee;
 select * from JDepartment;
 select * from JManager;
 select * from JProjects;
+select * from JCompany;
+select * from family;
 
 -- INNER JOIN / JOIN
 -- 1) Fetch the employee name and department name they belong to.
@@ -83,5 +111,41 @@ from JEmployee e
 left join JDepartment d on e.dept_id = d.dept_id
 join JManager m on m.manager_id = e.manager_id
 left join JProjects p on p.team_member_id = e.emp_id;
+
+-- FULL OUTER JOIN / FULL JOIN
+-- Full Join = Inner Join + All remaining records from Right table + All remaining
+-- 			   records from Left table.
+select e.emp_name, d.dept_name
+from JEmployee e
+full join JDepartment d 
+on d.dept_id = e.dept_id;
+
+-- CROSS JOIN [Cartesian Product]
+
+-- 5) Write a query to fetch the employee name and their corresponding department name.
+-- Also make sure to display the company name and the company location corresponding to
+-- each employee.
+select e.emp_name, d.dept_name, c.company_name, c.location
+from JEmployee e
+join JDepartment d
+on e.dept_id = d.dept_id
+cross join JCompany c;
+
+-- NATURAL JOIN [SQL will decide what the join condition] - Highly Not Recommended
+select e.emp_name, d.dept_name
+from JEmployee e
+natural join JDepartment d;
+
+-- SELF JOIN [When we join a table to itself]
+
+-- 6) Write a query to fetch the child name and their age corresponding to their parent
+-- name and parent age.
+select child.name as child_name, child.age as child_age,
+parent.name as parent_name, parent.age as parent_age
+from family as child
+join family as parent 
+on child.parent_id = parent.member_id;
+
+
 
 
