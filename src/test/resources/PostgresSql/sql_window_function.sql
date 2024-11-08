7c6a3b9e4c75bab95370425a9febd8f92031a7bd
JagatheshwaranN <jagatheshwaran.n29@gmail.com>
2024-11-08 19:55:30 +0530

    DB - PostgresSQL Sql Window Function Notes Updated

--- src/test/resources/PostgresSql/sql_window_function.sql
+++ src/test/resources/PostgresSql/sql_window_function.sql
@@ -21,7 +21,7 @@ select e.*, max(salary)
 over() as max_salary
 from employee e;
 
--- If we need to get the max salary corresponds to eacjh department, then we need
+-- If we need to get the max salary corresponds to each department, then we need
 -- to use the clause "partition by" followed by column name.
 select e.*, max(salary)
 over(partition by dept_name) as max_salary
@@ -50,4 +50,51 @@ select *
 from (select e.*, rank() over (partition by dept_name order by salary desc) as rank_order
 from employee e) x
 where x.rank_order < 4;
+
+-- Dense Rank
+-- Rank will skip a value for every duplicate records whereas Dense Rank will not
+-- skip a value.
+select *
+from (select e.*, rank() over (partition by dept_name order by salary desc) as rank_order,
+dense_rank() over (partition by dept_name order by salary desc) as dense_rank_order
+from employee e) x
+where x.rank_order < 4;
+
+-- Lag
+-- 3) Fetch a query to display if the salary of an employee is higher / lower /
+-- equal to the previous employee.
+select e.*,
+lag (salary) over (partition by dept_name order by emp_id) as prev_emp_salary
+from employee e;
+
+-- lag(salary, 2, 0)
+-- Here, 2 denotes the level, which means it will show the salary of employee
+-- whose is 2 level above the current employee.
+-- Here, 0 denotes the default value, for null values it will be replaces as 0.
+select e.*,
+lag (salary, 2, 0) over (partition by dept_name order by emp_id) as prev_emp_salary
+from employee e;
+
+-- Lead (Exact opposite of Lag)
+select e.*,
+lead (salary) over (partition by dept_name order by emp_id) as next_emp_salary
+from employee e;
+
+select e.*,
+lead (salary, 2, 0) over (partition by dept_name order by emp_id) as next_emp_salary
+from employee e;
+
+-- Use Case
+select e.*,
+lag (salary) over (partition by dept_name order by emp_id) as prev_emp_salary,
+case 
+when e.salary > lag(salary) over (partition by dept_name order by emp_id)
+then 'Salary Higher than previous employee'
+when e.salary < lag(salary) over (partition by dept_name order by emp_id)
+then 'Salary lower than previous employee'
+when e.salary = lag(salary) over (partition by dept_name order by emp_id)
+then 'Salary same as previous employee'
+end sal_range
+from employee e;
+
 
