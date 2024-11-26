e14e1a2ccb0851a6dffe8820819a41142800f203
JagatheshwaranN <jagatheshwaran.n29@gmail.com>
2024-11-26 08:39:58 +0530

    DB - Normalization Notes Updated

--- src/test/resources/PostgresSql/sql_normalization.sql
+++ src/test/resources/PostgresSql/sql_normalization.sql
@@ -42,3 +42,49 @@
 -- column (columns which are not part of the candidate key) should be fully dependent on all the
 -- columns. If there is any non-key column which depends only on one of the candidate key columns
 -- then this results in partial dependency.
+
+-- 3NF Rule
+-- 1. Must be in 2NF.
+-- 2. Avoid Transitive Dependencies.
+
+-- Transitive Dependency
+-- Let say you have a Table T which has 3 columns such as A, B, and C.
+-- If A is functionally dependent on B and B is functionally dependent on C then  we can say that A
+-- is functionally dependent on C.
+
+-- Insertion Anomalies
+-- Scenarios
+-- 1. Data Redundancy
+-- In a denormalized dataset, let say we have a customer who purchased 100 different products and those
+-- products have different order id and it has 100 records with the same customer details as duplicate
+-- information.
+
+-- But whereas in case of normalized dataset [3NF], we have only one record for the customer detail.
+
+-- 2. Missing Data
+-- In a denormalized dataset, let say we have a launched a new product and the detail is updated in 
+-- database, but only the product related fields are updated as it's a new product and no customer 
+-- have purchased it. So, order and customer related fields are null.
+
+-- But whereas in case of normalized dataset [3NF], we have separate table for products and there we 
+-- can add any new products.
+
+-- Deletion Anomalies
+-- In a denormalized dataset, if we want to delete the order which has been added wrongly. It will
+-- delete the order details but along with that, it will also delete the product and customer details
+-- as well because we have those data in the record to be deleted.
+
+-- But whereas in case of normalized dataset [3NF], we can delete the order without affecting the product 
+-- and customer detail.
+
+-- Update Anomalies
+-- In a denormalized dataset, if we want to update the product price of motorcycle from 200 to 250. We
+-- have to update more than one record which is not recommended and in realtime, it could be 1000 records.
+
+-- But whereas in case of normalized dataset [3NF], we can update the price of the product in one place under
+-- product table.
+
+
+
+
+
