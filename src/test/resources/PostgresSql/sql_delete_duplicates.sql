e96c7fd56b85fca1ba8d097ce914bb26a0448a33
JagatheshwaranN <jagatheshwaran.n29@gmail.com>
2024-12-05 10:01:58 +0530

    DB - Delete Duplicates Records In-Progress.

--- src/test/resources/PostgresSql/sql_delete_duplicates.sql
+++ src/test/resources/PostgresSql/sql_delete_duplicates.sql
@@ -94,5 +94,104 @@ group by model, brand);
 
 -- Solution 5: Using backup table.
 
+-- The below statement create the backup table as per the ex_cars table without the data.
+create table ex_cars_bkp 
+as select * 
+from ex_cars 
+where 1=2;
+
+select * from ex_cars_bkp;
+
+insert into ex_cars_bkp
+select *
+from ex_cars
+where id in (
+select min(id)
+from ex_cars
+group by model, brand);
+
+drop table ex_cars;
+
+alter table ex_cars_bkp rename to ex_cars;
+
+select * from ex_cars;
+
 -- Solution 6: Using backup table without dropping the original table.
 
+-- The below statement create the backup table as per the ex_cars table without the data.
+create table ex_cars_bkp 
+as select * 
+from ex_cars 
+where 1=2;
+
+select * from ex_cars_bkp;
+
+insert into ex_cars_bkp
+select *
+from ex_cars
+where id in (
+select min(id)
+from ex_cars
+group by model, brand);
+
+truncate table ex_cars;
+
+insert into ex_cars
+select * from ex_cars_bkp;
+
+drop table ex_cars_bkp;
+
+select * from ex_cars;
+
+-- ##### Delete the records even if the Id is duplicate. ##### --
+
+truncate table ex_cars;
+
+insert into ex_cars values (1, 'Model S', 'Tesla', 'Blue', 2018);
+insert into ex_cars values (2, 'EQS', 'Mercedes-Benz', 'Black', 2022);
+insert into ex_cars values (3, 'iX', 'BMW', 'Red', 2022);
+insert into ex_cars values (4, 'Ioniq 5', 'Hyundai', 'White', 2021);
+insert into ex_cars values (1, 'Model S', 'Tesla', 'Blue', 2018);
+insert into ex_cars values (4, 'Ioniq 5', 'Hyundai', 'White', 2021);
+
+select * from ex_cars;
+
+-- Solution 1: Delete using CTID [CTID available in Postgres SQL / ROWID available in Oracle]
+
+-- The above solution will not work in MySQL and Microsoft SQL Server.
+
+select *, ctid 
+from ex_cars;
+
+delete from ex_cars 
+where ctid in (
+select max(ctid) 
+from ex_cars
+group by model, brand
+having count(*) > 1);
+
+select * from ex_cars order by model, brand;
+
+-- Solution 2: By creating a temporary unique id column
+alter table ex_cars add column row_num int generated always as identity;
+
+select * from ex_cars order by model, brand;
+
+select max (row_num)
+from ex_cars
+group by model, brand
+having count(*) > 1;
+
+delete from ex_cars
+where row_num in
+(select max (row_num)
+from ex_cars
+group by model, brand
+having count(*) > 1);
+
+select * from ex_cars order by model, brand;
+
+alter table ex_cars drop column row_num;
+
+-- Solution 3: By creating a backup table
+-- Solution 4: By creating a backup table without dropping the original table
