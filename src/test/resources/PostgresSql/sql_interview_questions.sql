78c37c32defeeb9b21761a78e1403e83cd2c6100
JagatheshwaranN <jagatheshwaran.n29@gmail.com>
2024-12-08 09:01:38 +0530

    DB - Interview Questions Updated

--- src/test/resources/PostgresSql/sql_interview_questions.sql
+++ src/test/resources/PostgresSql/sql_interview_questions.sql
@@ -62,7 +62,7 @@ from cte
 group by buckets
 order by 1;
 
--- Leet Code Problem
+-- Leet Code Problem [Problem Statement 3]
 
 -- Table: Tree
 -- +-------------+------+
@@ -154,3 +154,84 @@ case when p_id is null then 'Root'
 	 else 'Leaf'
 end as type
 from tree;
+
+-- Problem Statement 4
+
+-- Dataset PostgreSQL
+drop table files;
+create table files
+(
+id              int primary key,
+date_modified   date,
+file_name       varchar(50)
+);
+insert into files values (1	,   to_date('2021-06-03','yyyy-mm-dd'), 'thresholds.svg');
+insert into files values (2	,   to_date('2021-06-01','yyyy-mm-dd'), 'redrag.py');
+insert into files values (3	,   to_date('2021-06-03','yyyy-mm-dd'), 'counter.pdf');
+insert into files values (4	,   to_date('2021-06-06','yyyy-mm-dd'), 'reinfusion.py');
+insert into files values (5	,   to_date('2021-06-06','yyyy-mm-dd'), 'tonoplast.docx');
+insert into files values (6	,   to_date('2021-06-01','yyyy-mm-dd'), 'uranian.pptx');
+insert into files values (7	,   to_date('2021-06-03','yyyy-mm-dd'), 'discuss.pdf');
+insert into files values (8	,   to_date('2021-06-06','yyyy-mm-dd'), 'nontheologically.pdf');
+insert into files values (9	,   to_date('2021-06-01','yyyy-mm-dd'), 'skiagrams.py');
+insert into files values (10,   to_date('2021-06-04','yyyy-mm-dd'), 'flavors.py');
+insert into files values (11,   to_date('2021-06-05','yyyy-mm-dd'), 'nonv.pptx');
+insert into files values (12,   to_date('2021-06-01','yyyy-mm-dd'), 'under.pptx');
+insert into files values (13,   to_date('2021-06-02','yyyy-mm-dd'), 'demit.csv');
+insert into files values (14,   to_date('2021-06-02','yyyy-mm-dd'), 'trailings.pptx');
+insert into files values (15,   to_date('2021-06-04','yyyy-mm-dd'), 'asst.py');
+insert into files values (16,   to_date('2021-06-03','yyyy-mm-dd'), 'pseudo.pdf');
+insert into files values (17,   to_date('2021-06-03','yyyy-mm-dd'), 'unguarded.jpeg');
+insert into files values (18,   to_date('2021-06-06','yyyy-mm-dd'), 'suzy.docx');
+insert into files values (19,   to_date('2021-06-06','yyyy-mm-dd'), 'anitsplentic.py');
+insert into files values (20,   to_date('2021-06-03','yyyy-mm-dd'), 'tallies.py');
+
+select * from files;
+
+-- A DB contains a list of filenames including their extensions and the dates they were last
+-- modified. For each date that a modification was made, return the date, the extension(s) 
+-- of the files that were modified the most, and the number of files modified that date. If
+-- more than one file extension ties for the most modifications, return them as a comma
+-- delimited list in reverse order.
+
+select * from files order by 2;
+
+-- Problem Breakdown
+-- 1. Fetch the file extension
+select * , position('.' in file_name)
+from files;
+
+select date_modified, substring(file_name, (position('.' in file_name)+1)) as file_ext
+from files;
+
+-- 2. For each day, how many times each file extension was modified
+select date_modified, substring(file_name, (position('.' in file_name)+1)) as file_ext, count(1) as cnt
+from files
+group by date_modified, file_ext
+order by 1;
+
+-- 3. For each day, fetch the most modified file extension from #2
+with cte as 
+(select date_modified, substring(file_name, (position('.' in file_name)+1)) as file_ext, count(1) as cnt
+from files
+group by date_modified, file_ext
+order by 1)
+select * 
+from cte c1
+where cnt = (select max(cnt) from cte c2
+where c2.date_modified = c1.date_modified);
+
+-- 4. If there is tie, then concatenate the multiple file extension
+with cte as 
+(select date_modified, substring(file_name, (position('.' in file_name)+1)) as file_ext, count(1) as cnt
+from files
+group by date_modified, file_ext
+order by 1)
+select date_modified, 
+string_agg(file_ext, ', ' order by file_ext desc) as extension,
+max(cnt) as count
+from cte c1
+where cnt = (select max(cnt) from cte c2
+where c2.date_modified = c1.date_modified)
+group by date_modified;
+
